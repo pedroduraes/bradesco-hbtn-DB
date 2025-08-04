565223d569e417574af21c8904b997b5d4686640
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-04 20:36:25 +0000

    0. Criar uma aplicação CRUD para Gestão de Cursos usando Hibernate + JPA + SQLite

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
@@ -10,12 +10,13 @@ public class GestaoCursosMain {
 
         public static void connect() {
         // connection string
+        //var url = "jdbc:sqlite:database_admin_jpa.db";
         var url = "jdbc:sqlite:database_admin_jpa.db";
 
         try (var conn = DriverManager.getConnection(url)) {
             System.out.println("Connection to SQLite has been established.");
         } catch (SQLException e) {
-            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
     }
 }
