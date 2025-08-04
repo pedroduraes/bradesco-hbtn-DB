a14cf0ca41d749af03f89b313c9a9ee03bfb03c1
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-04 20:54:40 +0000

    0. Criar uma aplicação CRUD para Gestão de Cursos usando Hibernate + JPA + SQLite

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/demo/GestaoCursosMain.java
@@ -1,5 +1,7 @@
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class GestaoCursosMain {
     public static void main( String[] args )
@@ -15,6 +17,24 @@ public class GestaoCursosMain {
 
         try (var conn = DriverManager.getConnection(url)) {
             System.out.println("Connection to SQLite has been established.");
+
+            Statement statement = conn.createStatement();
+            statement.setQueryTimeout(30);  // Espera so por 30 segundos para conectar
+
+            // Roda os comandos para o SQLite
+            statement.executeUpdate("DROP TABLE IF EXISTS terminalroot");
+            statement.executeUpdate("CREATE TABLE terminalroot (id INTEGER, name STRING)");
+            statement.executeUpdate("INSERT INTO terminalroot VALUES(1, 'Marcos Oliveira')");
+            statement.executeUpdate("INSERT INTO terminalroot VALUES(2, 'James Gosling')");
+            ResultSet rs = statement.executeQuery("SELECT * FROM terminalroot");
+            while(rs.next()) {
+                // Ler os dados inseridos
+                System.out.println("NOME DO CARA  : " + rs.getString("name"));
+                System.out.println("IDENTIFICACAO : " + rs.getInt("id"));
+            }
+            
+
+
         } catch (SQLException e) {
             e.printStackTrace();
         }
