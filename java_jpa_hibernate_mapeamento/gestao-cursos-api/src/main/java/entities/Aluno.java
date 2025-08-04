53baf67ccfd0166862300368d7abba8d18e3a50b
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-04 19:36:42 +0000

    0. Criar uma aplicação CRUD para Gestão de Cursos usando Hibernate + JPA + SQLite

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Aluno.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Aluno.java
@@ -1,5 +1,42 @@
 package entities;
 
 public class Aluno {
+ 
+    private int id;
+    private  String nomeCompleto;
+    private String matricula;
+    private Date nascimento
+    private String email;
+    
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+    public String getMatricula() {
+        return matricula;
+    }
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+    public Date getNascimento() {
+        return nascimento;
+    }
+    public void setNascimento(Date nascimento) {
+        this.nascimento = nascimento;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
     
 }
