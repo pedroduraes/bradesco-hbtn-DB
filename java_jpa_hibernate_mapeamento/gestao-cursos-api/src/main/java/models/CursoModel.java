3c475bec510a599c6d4701d8cd9370261c16545f
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-04 19:45:34 +0000

    0. Criar uma aplicação CRUD para Gestão de Cursos usando Hibernate + JPA + SQLite

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/models/CursoModel.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/models/CursoModel.java
@@ -1,5 +1,28 @@
 package models;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import entities.Curso;
+
 public class CursoModel {
-    
+    public void create(Curso curso) {
+               EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
+        EntityManager em = emf.createEntityManager();
+
+        try {
+            System.out.println("Iniciando a transação");
+            em.getTransaction().begin();
+            em.persist(curso);
+            em.getTransaction().commit();
+            System.out.println("Curso criado com sucesso !!!");
+        } catch (Exception e) {
+            em.close();
+            System.err.println("Erro ao criar um curso !!!" + e.getMessage());
+        } finally {
+            em.close();
+            System.out.println("Finalizando a transação");
+        }
+    }
 }
