f5c95ae5cb180e9164319586117362eadefa3699
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-05 14:54:42 +0000

    0. Criar uma aplicação CRUD para Gestão de Cursos usando Hibernate + JPA + SQLite

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Aluno.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/entities/Aluno.java
@@ -3,17 +3,21 @@ package entities;
 import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity
 public class Aluno {
- 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    private  String nomeCompleto;
+    private String nomeCompleto;
     private String matricula;
     private Date nascimento;
     private String email;
 
-    
     public Aluno(int id, String nomeCompleto, String matricula, Date nascimento, String email) {
         this.id = id;
         this.nomeCompleto = nomeCompleto;
@@ -21,37 +25,45 @@ public class Aluno {
         this.nascimento = nascimento;
         this.email = email;
     }
-    
+
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getNomeCompleto() {
         return nomeCompleto;
     }
+
     public void setNomeCompleto(String nomeCompleto) {
         this.nomeCompleto = nomeCompleto;
     }
+
     public String getMatricula() {
         return matricula;
     }
+
     public void setMatricula(String matricula) {
         this.matricula = matricula;
     }
+
     public Date getNascimento() {
         return nascimento;
     }
+
     public void setNascimento(Date nascimento) {
         this.nascimento = nascimento;
     }
+
     public String getEmail() {
         return email;
     }
+
     public void setEmail(String email) {
         this.email = email;
     }
 
-        
 }
