6778525e046d12484b31c47c9843575a47a4bb48
Pedro Henrique Martins Duraes <pedro.duraes@bradesco.com.br>
2025-08-05 20:09:41 +0000

    0. Criar uma aplicação CRUD para Gestão de Cursos usando Hibernate + JPA + SQLite

--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/models/AlunoModel.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/models/AlunoModel.java
@@ -1,47 +1,78 @@
 package models;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 
 import entities.Aluno;
 
 public class AlunoModel {
 
+	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
+	
     public void create(Aluno aluno) {
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
-        EntityManager em = emf.createEntityManager();
+    	
+        EntityManager em = emf.createEntityManager();        
 
         try {
-            System.out.println("Iniciando a transação");
+            System.out.println("Iniciando a transaÃ§Ã£o");
             em.getTransaction().begin();
             em.persist(aluno);
             em.getTransaction().commit();
-            System.out.println("Aluno criado com sucesso !!!");
+            System.out.println("Aluno criado com sucesso id:" + em.find(Aluno.class, 1L).getId());
         } catch (Exception e) {
             em.close();
             System.err.println("Erro ao criar um aluno !!!" + e.getMessage());
         } finally {
             em.close();
-            System.out.println("Finalizando a transação");
+            System.out.println("Finalizando a transaÃ§Ã£o");
         }
     }
 
     public Aluno findById(Long id) {
-        // TODO
-        return null;
+        EntityManager em = emf.createEntityManager();
+        return em.find(Aluno.class, id);
     }
 
     public  List<Aluno> findAll() {
-        // TODO
-        return null;
+        EntityManager em = emf.createEntityManager();
+    	TypedQuery<Aluno> query = em.createQuery("SELECT a FROM Aluno a", Aluno.class);
+        // Execute the query and return the result list
+        return query.getResultList();
     }
 
     public void update(Aluno aluno) {
-        // TODO
+    	EntityManager em = emf.createEntityManager();
+    	try {
+    		em.getTransaction().begin();
+    		em.merge(aluno);	
+    		em.getTransaction().commit();
+		} catch (Exception e) {
+			em.close();
+			System.err.println("Erro ao atualizar aluno !!!" + e.getMessage());
+		}
+    	finally {
+    		em.close();
+    	}
+
     }
 
     public void delete(Aluno aluno) {
-        // TODO
+    	EntityManager em = emf.createEntityManager();
+    	try {
+    		Aluno alunoToDelete = em.find(Aluno.class, aluno.getId());
+    		em.getTransaction().begin();
+    		em.remove(alunoToDelete);	
+    		em.getTransaction().commit();
+		} catch (Exception e) {
+			em.close();
+			System.err.println("Erro ao excluir aluno !!!" + e.getMessage());
+		}
+    	finally {
+    		em.close();
+    	}
     }
 }
--- java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/models/CursoModel.java
+++ java_jpa_hibernate_mapeamento/gestao-cursos-api/src/main/java/models/CursoModel.java
@@ -1,48 +1,78 @@
 package models;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
-
+import javax.persistence.TypedQuery;
 
 import entities.Curso;
 
 public class CursoModel {
-    public void create(Curso curso) {
-               EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
-        EntityManager em = emf.createEntityManager();
+	
+	private EntityManagerFactory emf = Persistence.createEntityManagerFactory("gestao-cursos-jpa");
+	
+	public void create(Curso curso) {
+		
+		EntityManager em = emf.createEntityManager();
 
-        try {
-            System.out.println("Iniciando a transação");
-            em.getTransaction().begin();
-            em.persist(curso);
-            em.getTransaction().commit();
-            System.out.println("Curso criado com sucesso !!!");
-        } catch (Exception e) {
-            em.close();
-            System.err.println("Erro ao criar um curso !!!" + e.getMessage());
-        } finally {
-            em.close();
-            System.out.println("Finalizando a transação");
-        }
-    }
+		try {
+			System.out.println("Iniciando a transaÃ§Ã£o");
+			em.getTransaction().begin();
+			em.persist(curso);
+			em.getTransaction().commit();
+			System.out.println("Curso criado com sucesso !!!" + curso.getId());
+		} catch (Exception e) {
+			em.close();
+			System.err.println("Erro ao criar um curso !!!" + e.getMessage());
+		} finally {
+			em.close();
+			System.out.println("Finalizando a transaÃ§Ã£o");
+		}
+	}
 
-        public Curso findById(Long id) {
-        // TODO
-        return null;
-    }
+	public Curso findById(Long id) {
+		EntityManager em = emf.createEntityManager();
+        return em.find(Curso.class, id);
+	}
 
-    public  List<Curso> findAll() {
-        // TODO
-        return null;
-    }
+	public List<Curso> findAll() {
+		EntityManager em = emf.createEntityManager();
+    	TypedQuery<Curso> query = em.createQuery("SELECT a FROM Curso a", Curso.class);
+        // Execute the query and return the result list
+        return query.getResultList();
+	}
 
-    public void update(Curso curso) {
-        // TODO
-    }
+	public void update(Curso curso) {
+		EntityManager em = emf.createEntityManager();
+    	try {
+    		em.getTransaction().begin();
+    		em.merge(curso);	
+    		em.getTransaction().commit();
+		} catch (Exception e) {
+			em.close();
+			System.err.println("Erro ao atualizar curso!!!" + e.getMessage());
+		}
+    	finally {
+    		em.close();
+    	}
+	}
 
-    public void delete(Curso curso) {
-        // TODO
-    }
+	public void delete(Curso curso) {
+		EntityManager em = emf.createEntityManager();
+    	try {
+    		Curso cursoToDelete = em.find(Curso.class, curso.getId());
+    		em.getTransaction().begin();
+    		em.remove(cursoToDelete);	
+    		em.getTransaction().commit();
+		} catch (Exception e) {
+			em.close();
+			System.err.println("Erro ao excluir curso !!!" + e.getMessage());
+		}
+    	finally {
+    		em.close();
+    	}
+	}
 
 }
